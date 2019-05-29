e52c0a3bed7820988e9da463ed2492d67d1e7302
Andreas Weinzierl <20560137+aWeinzierl@users.noreply.github.com>
2019-05-29 20:26:48 +0200

    first draft of install script

---
+++ installation.bash
@@ -0,0 +1,19 @@
+# preqrequisites
+#Java 
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get install openjdk-8-jdk
+
+cd ..
+source ../devel/setup.bash
+rosdep update --rosdistro indigo
+wstool init .
+wstool merge https://raw.github.com/knowrob/knowrob/indigo/rosinstall/knowrob-base.rosinstall
+wstool update
+rosdep install --ignore-src --from-paths . --rosdistro indigo
+cd ..
+catkin_make
+
+
+# install additional dependencies
+sudo apt-get install ros-indigo-rosbridge-suite
+
