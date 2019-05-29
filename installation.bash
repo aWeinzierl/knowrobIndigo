0fbc3dbe70bd080767ce3d27deac82c2a6717ddf
Andreas Weinzierl <20560137+aWeinzierl@users.noreply.github.com>
2019-05-29 21:46:22 +0200

    first success

--- installation.bash
+++ installation.bash
@@ -4,15 +4,20 @@ sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk
 
-cd ..
+cd ../..
+catkin_make
+cd src
 source ../devel/setup.bash
 rosdep update --include-eol-distros
+rm .rosinstall
+rm .rosinstall.bak
 wstool init .
-wstool merge https://raw.github.com/knowrob/knowrob/indigo/rosinstall/knowrob-base.rosinstall
+wstool merge https://raw.github.com/knowrob/knowrob/master/rosinstall/knowrob-base.rosinstall
 wstool update
-rosdep install --ignore-src --from-paths . --rosdistro indigo
+rosdep install --ignore-src --from-paths .
 cd ..
 catkin_make
+source devel/setup.bash
 
 
 # install additional dependencies
